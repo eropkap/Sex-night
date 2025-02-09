be4b0fe47fd041aedfec5345fd400523245a3bee
eropkap <eropkap@icloud.com>
2025-02-09 09:12:30 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -35,15 +35,13 @@ function spinWheel() {
     let bodyLocation = bodyLocations[Math.floor(Math.random() * bodyLocations.length)];
 
     let wheel = document.getElementById("wheel");
-    let cardContainer = document.getElementById("cardContainer");
     let card = document.getElementById("card");
 
     wheel.classList.add("spin");
     setTimeout(() => {
         wheel.classList.remove("spin");
-        cardContainer.classList.remove("hidden");
-        card.classList.add("flip");
-        card.querySelector(".card-back").innerHTML = `<h2>${task}</h2><p>Финал: ${finalLocation} – ${bodyLocation}</p>`;
+        card.innerHTML = `<h2>${task}</h2><p>Финал: ${finalLocation} – ${bodyLocation}</p>`;
+        card.classList.add("show");
     }, 2000);
 }
 
